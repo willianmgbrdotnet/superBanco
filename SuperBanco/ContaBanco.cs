6c2ba6698a221e7ab33607a3f47f0ac6f1619016
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-06-07 13:53:50 -0300

    Classe Transação e comentários no Código

--- SuperBanco/ContaBanco.cs
+++ SuperBanco/ContaBanco.cs
@@ -7,15 +7,38 @@ namespace SuperBanco
 {
     public class ContaBanco
     {
+        //Apenas "get" pois o usuário não vai "setar" o numero da conta.
         public string Numero { get; }
         public string Dono { get; set; }
         public decimal Saldo { get; set; }
 
+        public static int numeroConta = 123450;
+
+        
+        //Método Construtor para gerar um novo Objeto deste tipo(classe)
         public ContaBanco(string nome, decimal valor)
         {
             this.Dono = nome;
             this.Saldo = valor;
+
+            //Auto-gera um novo numero de conta para cada novo Objeto
+            numeroConta++;
+
+            //Converterá numeroConta de int para string, uma vez que a 
+            //propriedade Numero é do tipo string
+            this.Numero = numeroConta.ToString();
+        }
+
+        //Métodos ou funções da Conta
+        public void Depositar()
+        {
             
         }
+        public void Sacar()
+        {
+            
+        }
+
+
     }
 }
--- SuperBanco/Program.cs
+++ SuperBanco/Program.cs
@@ -6,8 +6,10 @@ namespace SuperBanco
     {
         static void Main(string[] args)
         {
+            //Criado um novo Objeto do tipo ContaBanco, sendo obrigado a 
+            //setar o nome e o valor no momento da criação da conta. 
             ContaBanco contaB = new ContaBanco("Diogo", 10000);
-            System.Console.WriteLine($"A conta {contaB.Numero} de {contaB.Dono} foi criada com {contaB.Saldo} dinheiros");
+            Console.WriteLine($"A conta {contaB.Numero} de {contaB.Dono} foi criada com {contaB.Saldo} dinheiros");
 
         }
         
